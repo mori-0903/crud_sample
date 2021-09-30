e9505b8b2aa3a886244f186bfa6c06b3bfc43e4b
R.moriyama <moriforest537@gmail.com>
2021-09-30 15:56:30 +0900

    wip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
 
   def create
     User.create(user_params)
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/users", to: "users#index"
   get "/users/new", to: "users#new"
   post "/users", to: "users#create"
+  get "/users/:id/edit", to: "users#edit"
+  delete "/users/:id", to: "users#destroy"
 end
