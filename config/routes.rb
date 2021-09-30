3c479be5e47f2bf8b21f7b6cd4db88337ec0ccdf
R.moriyama <moriforest537@gmail.com>
2021-09-30 16:31:31 +0900

    resouces を使用した形式に変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
   get "/users", to: "users#index"
   get "/users/new", to: "users#new"
   post "/users", to: "users#create"
